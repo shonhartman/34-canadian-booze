20880c221beddecc548b4cbd10657f3c7645215f
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-27 15:35:16 -0500

    ...

--- app/app.js
+++ app/app.js
@@ -2,19 +2,17 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
 
-import account from './modules/account';
-import chats from './modules/chats';
-import dash from './modules/dash';
-import tabs from './modules/tabs';
+import products from './modules/products';
+
 
 let App = angular.module('app', [
   'ionic',
   'ui.router',
-  
+
 ]);
 
 function config($urlRouterProvider) {
-  $urlRouterProvider.otherwise("/tab/dash");
+  $urlRouterProvider.otherwise("/products");
 }
 
 App.config(config);
